7ef42520aa55ab0d8f98fa1a8df1e0fada435187
Ana Neri <ananeri32@hotmail.com>
2022-01-29 16:35:56 -0300

    feat(ModelMapper): adding as a dependency
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- src/main/java/com/anabneri/registrationschool/RegistrationSchoolMetadataTestApplication.java
+++ src/main/java/com/anabneri/registrationschool/RegistrationSchoolMetadataTestApplication.java
@@ -1,10 +1,18 @@
 package com.anabneri.registrationschool;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RegistrationSchoolMetadataTestApplication {
+
+	// singleton instance
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationSchoolMetadataTestApplication.class, args);
--- src/main/java/com/anabneri/registrationschool/controller/StudentController.java
+++ src/main/java/com/anabneri/registrationschool/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.anabneri.registrationschool.controller;
 import com.anabneri.registrationschool.model.StudentDTO;
 import com.anabneri.registrationschool.model.entity.Student;
 import com.anabneri.registrationschool.service.StudentService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,25 +13,21 @@ public class StudentController {
 
 
     private StudentService studentService;
+    private ModelMapper modelMapper;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, ModelMapper modelMapper) {
         this.studentService = studentService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public StudentDTO create( @RequestBody StudentDTO studentDTO ) {
 
-        Student entity = Student.builder()
-                .studentName(studentDTO.getStudentName())
-                .dateOfRegistration(studentDTO.getDateOfRegistration()).build();
-
+        Student entity =  modelMapper.map(studentDTO, Student.class);
         entity = studentService.save(entity);
 
-        return StudentDTO.builder()
-                .studentId(entity.getStudentId())
-                .studentName(entity.getStudentName())
-                .dateOfRegistration(entity.getDateOfRegistration()).build();
+        return modelMapper.map(entity, StudentDTO.class);
     }
 
 }
