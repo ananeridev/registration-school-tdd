1b29165926e98cda114c818921d92d64e04ae54f
Ana Neri <ananeri32@hotmail.com>
2022-01-29 11:02:29 -0300

    initial commit
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

---
+++ src/main/java/com/anabneri/registrationschool/Registration/School/Metadata/Test/RegistrationSchoolMetadataTestApplication.java
@@ -0,0 +1,13 @@
+package com.anabneri.registrationschool.Registration.School.Metadata.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSchoolMetadataTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSchoolMetadataTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anabneri/registrationschool/Registration/School/Metadata/Test/RegistrationSchoolMetadataTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabneri.registrationschool.Registration.School.Metadata.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSchoolMetadataTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
