1e56cbe7a93a9f6bc2e628bab35cb1a7dc52d8f7
Ana Neri <ananeri32@hotmail.com>
2022-02-01 19:38:54 -0300

    feat(test): creating integration test for repository
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- src/main/java/com/anabneri/registrationschool/model/entity/Student.java
+++ src/main/java/com/anabneri/registrationschool/model/entity/Student.java
@@ -5,14 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @Column(name = "student_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer studentId;
+
+    @Column(name = "student_name")
     private String studentName;
+
+    @Column(name = "date_of_registration")
     private String dateOfRegistration;
+
+    @Column
     private String registration;
 }
---
+++ src/test/java/com/anabneri/registrationschool/repository/StudentRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.anabneri.registrationschool.repository;
+
+import com.anabneri.registrationschool.model.entity.Student;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class StudentRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    StudentRepository repository;
+
+    @Test
+    @DisplayName("Should return true when exists an student with a registration already created.")
+    public void returnTrueWhenRegistrationExists() {
+
+        String registration = "123";
+        Student student = Student.builder().studentName("Ana Neri").dateOfRegistration("10/10/2021").registration(registration).build();
+        entityManager.persist(student);
+
+        boolean exists = repository.existsByRegistration(registration);
+
+        assertThat(exists).isTrue();
+    }
+
+}
